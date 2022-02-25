65dea5ab828378d13b5dcd69ab74720749c21b4b
gFireice <80753741+gFireice@users.noreply.github.com>
2022-02-25 20:33:41 +0300

    Add files via upload

---
+++ ServiceOfEquipment_Yakunin-master/ServiceOfEquioment_Yakunin/HelpClass/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceOfEquipment_Yakunin.HelpClass
+{
+    internal class AppData
+    {
+        public static EF.Entities Context { get; } = new EF.Entities();
+    }
+}
